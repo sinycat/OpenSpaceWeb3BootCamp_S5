4312211ff1896ee2507d1f161e63102c0e92bc9a
sinycat <sinycat888@gmail.com>
2025-02-13 18:29:24 +0800

    day3 modified

--- day3/Bank.sol
+++ day3/Bank.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
-// 合约已部署 sepolia测试网 地址:0x8B82B4056487A92C9c7FE6c0d433a2411103D945
 contract Bank {
+
     // 定义管理员地址
     address public admin;
 
@@ -15,10 +14,9 @@ contract Bank {
         address user;
         uint256 amount;
     }
-    UserDeposit[3] public topDepositors;
 
-    // 构造函数，初始化管理员地址为合约部署者
-    constructor() {
+    // 构造函数，初始化管理员地址为合约部署者，设置为 payable 以允许部署时接收以太币
+    constructor() payable {
         admin = msg.sender;
     }
 
@@ -30,38 +28,37 @@ contract Bank {
 
     // 接收以太币的函数，允许直接向合约地址存款
     receive() external payable {
-        updateDeposit(msg.sender, msg.value);
+        deposits[msg.sender] += msg.value;
     }
 
-    // 内部函数，更新存款信息和前 3 名用户
-    function updateDeposit(address _user, uint256 _amount) internal {
-        deposits[_user] += _amount;
-
-        // 更新前 3 名用户
-        for (uint256 i = 0; i < 3; i++) {
-            if (deposits[_user] > topDepositors[i].amount) {
-                // 将排名靠后的用户依次后移
-                for (uint256 j = 2; j > i; j--) {
-                    topDepositors[j] = topDepositors[j - 1];
-                }
-                topDepositors[i] = UserDeposit(_user, deposits[_user]);
-                break;
+    // 内部函数，获取所有有存款的用户地址
+    function getAllUsers() internal view returns (address[] memory) {
+        address[] memory tempUsers = new address[](address(this).balance > 0 ? address(this).balance : 1);
+        uint256 count = 0;
+        for (uint256 i = 0; i < tempUsers.length; i++) {
+            address user = address(uint160(i));
+            if (deposits[user] > 0) {
+                tempUsers[count] = user;
+                count++;
             }
         }
+        address[] memory users = new address[](count);
+        for (uint256 i = 0; i < count; i++) {
+            users[i] = tempUsers[i];
+        }
+        return users;
     }
 
-    // 管理员提取自定义金额资金到自己账户，输入单位为 ether
-    function withdrawToSelf(uint256 _amountInEther) external onlyAdmin {
-        uint256 amountInWei = _amountInEther * 1 ether;
-        require(amountInWei <= address(this).balance, "Insufficient balance in the contract");
-        payable(admin).transfer(amountInWei);
+    // 管理员提取自定义金额资金到自己账户，输入单位为 wei
+    function withdrawToSelf(uint256 _amountInWei) external onlyAdmin {
+        require(_amountInWei <= address(this).balance, "Insufficient balance in the contract");
+        payable(admin).transfer(_amountInWei);
     }
 
-    // 管理员向指定地址转账自定义金额，输入单位为 ether
-    function withdrawToAddress(address payable _recipient, uint256 _amountInEther) external onlyAdmin {
-        uint256 amountInWei = _amountInEther * 1 ether;
-        require(amountInWei <= address(this).balance, "Insufficient balance in the contract");
-        _recipient.transfer(amountInWei);
+    // 管理员向指定地址转账自定义金额，输入单位为 wei
+    function withdrawToAddress(address payable _recipient, uint256 _amountInWei) external onlyAdmin {
+        require(_amountInWei <= address(this).balance, "Insufficient balance in the contract");
+        _recipient.transfer(_amountInWei);
     }
 
     // 获取指定地址的存款金额
@@ -69,8 +66,30 @@ contract Bank {
         return deposits[_user];
     }
 
-    // 获取前 3 名存款用户
+    // 获取前 3 名存款用户，调用时进行排序
     function getTopDepositors() external view returns (UserDeposit[3] memory) {
-        return topDepositors;
+        UserDeposit[3] memory top;
+        address[] memory allUsers = getAllUsers();
+
+        for (uint256 i = 0; i < allUsers.length; i++) {
+            address user = allUsers[i];
+            uint256 userDeposit = deposits[user];
+            for (uint256 j = 0; j < 3; j++) {
+                if (userDeposit > top[j].amount) {
+                    for (uint256 k = 2; k > j; k--) {
+                        top[k] = top[k - 1];
+                    }
+                    top[j] = UserDeposit(user, userDeposit);
+                    break;
+                }
+            }
+        }
+
+        return top;
+    }
+
+    // 获取合约账户以太坊的数量
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
